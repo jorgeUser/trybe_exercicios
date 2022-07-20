ca67f7a31b867867213c2d905c825042ab1d4278
jorgeuser <jorge.araujo.kgb@gmail.com>
2022-07-20 18:29:46 -0300

    programa que descobre maior número entre 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/maiorDeTres.js
@@ -0,0 +1,12 @@
+const a= 1000;
+const b=800;
+const c=102;
+
+if(a>b && a>c){
+    console.log("o maior número é o A!")
+}else if(b>a && b >c){
+    console.log("O maior número é o B!")
+    
+}else if(c>a && c>b){
+    console.log("O maior número é o C!")
+}
