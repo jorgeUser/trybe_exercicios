006205d807a9a73601778ec7bbbe3f5a3f869d46
jorgeuser <jorge.araujo.kgb@gmail.com>
2022-07-20 20:22:43 -0300

    exercício 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/angulosTriangulo.js
@@ -0,0 +1,12 @@
+const a= 45;
+const b=-91;
+const c=45;
+const soma=a+b+c;
+
+if(soma==180 && soma >0){
+   console.log(true)
+}else if(soma!=180 && soma >= 0){
+    console.log(false)
+}else if(soma<0){
+    console.log("ângulo invalido!!!")
+}
