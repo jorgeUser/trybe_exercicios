120fab7cc98c1777a2a2a3efb62494a208cbf61c
jorgeuser <jorge.araujo.kgb@gmail.com>
2022-07-20 18:54:04 -0300

    exercicio 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/positNegatNulo.js
@@ -0,0 +1,9 @@
+const numero= 0;
+
+if (numero>=1){
+    console.log("Esse é um número positivo!")
+} else if (numero<1 && numero != 0){
+console.log("Esse é um numero negativo!")
+}else if(numero === 0){
+    console.log("Esse número é 0 !")
+}
